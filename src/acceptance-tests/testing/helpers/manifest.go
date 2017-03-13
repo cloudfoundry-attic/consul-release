92b23dfdb857005e6df01defed0ecaf8efb1a68f
Genevieve LEsperance <glesperance@pivotal.io>
2017-03-13 15:10:00 -0700

    Update scaling tests for iaas-agnostic manifest
    [#134181451]
    
    Signed-off-by: Christian Ang <cang@pivotal.io>

--- src/acceptance-tests/testing/helpers/manifest.go
+++ src/acceptance-tests/testing/helpers/manifest.go
@@ -41,3 +41,35 @@ func GetVMsFromManifest(manifest consul.ManifestV2) []bosh.VM {
 
 	return vms
 }
+
+func GetVMsFromManifestV2(manifest string) []bosh.VM {
+	var vms []bosh.VM
+
+	instanceGroups, err := ops.InstanceGroups(manifest)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, ig := range instanceGroups {
+		for i := 0; i < ig.Instances; i++ {
+			vms = append(vms, bosh.VM{JobName: ig.Name, Index: i, State: "running"})
+		}
+	}
+
+	return vms
+}
+
+func GetVMIPs(boshClient bosh.Client, deploymentName, jobName string) ([]string, error) {
+	vms, err := boshClient.DeploymentVMs(deploymentName)
+	if err != nil {
+		return []string{}, err
+	}
+
+	for _, vm := range vms {
+		if vm.JobName == jobName {
+			return vm.IPs, nil
+		}
+	}
+
+	return []string{}, nil
+}
